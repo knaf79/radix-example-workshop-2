48b9122ab694f0267d8d2d44c9e53c7fd0bbf31a
Knut André Furuløkken <KNAF@equinor.com>
2019-09-17 15:04:13 +0200

    Switch to using echo backend

--- www/src/components/Header.js
+++ www/src/components/Header.js
@@ -4,15 +4,16 @@ import logo from './logo.svg';
 const Header = () => {
   return <header className="App-header">
     <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Radix a PaaS built on Kubernetes
-    </p>
+    
     <div className="App-description">
     <br/>
     Radix is an application platform built to increase development speed by providing  
     <br />
     the best possible tools to develop and run your applications.
     </div>
+    <p>
+      Laurent
+    </p>
     <a
       className="App-link"
       href="https://www.radix.equinor.com"
